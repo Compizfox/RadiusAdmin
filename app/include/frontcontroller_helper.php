05d1ffa66d0133deb7e94b0f70f2224b73dbef0c
Compizfox <7zQf*YF8dmDR>
2016-01-20 14:50:18 +0100

    Copyright header

--- app/include/frontcontroller_helper.php
+++ app/include/frontcontroller_helper.php
@@ -1,28 +1,28 @@
 <?php
-/*
-	Filename:   frontcontroller_helper.php
-	Date:       2015-04-27
-	Author:     Lars Veldscholte
-	            lars@veldscholte.eu
-	            http://lars.veldscholte.eu
+/**
+ 	Filename:   frontcontroller_helper.php
+ 	Date:       2016-01-20
+ 	Author:     Lars Veldscholte
+ 	            lars@veldscholte.eu
+ 	            http://lars.veldscholte.eu
 
-	Copyright 2015 Lars Veldscholte
+ 	Copyright 2016 Lars Veldscholte
 
-	This file is part of RadiusAdmin.
+ 	This file is part of RadiusAdmin.
 
-	RadiusAdmin is free software: you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation, either version 3 of the License, or
-	(at your option) any later version.
+ 	RadiusAdmin is free software: you can redistribute it and/or modify
+ 	it under the terms of the GNU General Public License as published by
+ 	the Free Software Foundation, either version 3 of the License, or
+ 	(at your option) any later version.
 
-	RadiusAdmin is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
+ 	RadiusAdmin is distributed in the hope that it will be useful,
+ 	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ 	GNU General Public License for more details.
 
-	You should have received a copy of the GNU General Public License
-	along with RadiusAdmin. If not, see <http://www.gnu.org/licenses/>.
-*/
+ 	You should have received a copy of the GNU General Public License
+ 	along with RadiusAdmin. If not, see <http://www.gnu.org/licenses/>.
+ */
 
 ini_set("display_errors", 1);
 error_reporting(E_ALL);
--- public_html/js/avp_table.js
+++ public_html/js/avp_table.js
@@ -1,27 +1,27 @@
 /*
-	Filename:   avp_table.js
-	Date:       2015-05-15
-	Author:     Lars Veldscholte
-	            lars@veldscholte.eu
-	            http://lars.veldscholte.eu
-
-	Copyright 2015 Lars Veldscholte
-
-	This file is part of RadiusAdmin.
-
-	RadiusAdmin is free software: you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation, either version 3 of the License, or
-	(at your option) any later version.
-
-	RadiusAdmin is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
-
-	You should have received a copy of the GNU General Public License
-	along with RadiusAdmin. If not, see <http://www.gnu.org/licenses/>.
-*/
+ * 	Filename:   avp_table.js
+ * 	Date:       2016-01-20
+ * 	Author:     Lars Veldscholte
+ * 	            lars@veldscholte.eu
+ * 	            http://lars.veldscholte.eu
+ *
+ * 	Copyright 2016 Lars Veldscholte
+ *
+ * 	This file is part of RadiusAdmin.
+ *
+ * 	RadiusAdmin is free software: you can redistribute it and/or modify
+ * 	it under the terms of the GNU General Public License as published by
+ * 	the Free Software Foundation, either version 3 of the License, or
+ * 	(at your option) any later version.
+ *
+ * 	RadiusAdmin is distributed in the hope that it will be useful,
+ * 	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * 	GNU General Public License for more details.
+ *
+ * 	You should have received a copy of the GNU General Public License
+ * 	along with RadiusAdmin. If not, see <http://www.gnu.org/licenses/>.
+ */
 
 $(document).ready(function() {
 	$('#checkattrs-table').on("click", '#delete-row', function() {
